cbb46328132516a69beb3ca2ea2f792d752a7e6e
David Maiato <142182549+dmaiato@users.noreply.github.com>
2025-08-19 20:57:32 -0300

    Add files via upload

---
+++ Aula02/src/index.js
@@ -0,0 +1,98 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+// const router = express.Router();
+
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
+// router.use((req, res, next) => {
+//   console.log("Middleware");
+//   next();
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+const products = [
+  {
+    id: 1,
+    name: "Motherboard",
+    price: 250,
+  },
+  {
+    id: 2,
+    name: "CPU",
+    price: 300,
+  },
+  {
+    id: 3,
+    name: "RAM",
+    price: 150,
+  },
+  {
+    id: 4,
+    name: "GPU 1",
+    price: 400,
+  },
+  {
+    id: 5,
+    name: "GPU 2",
+    price: 450,
+  },
+];
+
+/*
+PAGINAS ESTATICAS
+*/
+
+app.use(express.static("public"));
+
+/*
+TEMPLATES
+*/
+
+app.get("/products", (req, res) => {
+  res.render("products-list", { products });
+});
+
+app.get("/products/{:id}", (req, res) => {
+  res.render("product-page", { product: products[req.params.id - 1] });
+});
+
+// app.get('/products/:id', (req, res) => {
+//     id = req.params.id
+//     console.log(typeof(id))
+//     if (id > products.length) {
+//         res.write('ERROR: index out of bouds')
+//         return
+//     }
+//     res.json(products[id])
+// })
+
+/*
+Rotas com Parâmetros e Query Params
+
+Crie um endpoint GET /produtos/:id que retorna detalhes de um produto.
+
+Crie um endpoint GET /busca que recebe um query param nome e retorna apenas os produtos que contêm esse nome.
+
+Exemplo:
+GET /busca?nome=camisa
+*/
+
+/*
+Middleware
+
+Crie um middleware que:
+
+Registra no console cada requisição feita (rota, método e hora).
+
+Bloqueia acesso a certas rotas (POST /products) se não tiver um header Authorization válido.
+*/
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
